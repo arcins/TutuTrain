8c23a8cd76b0f12c624bc150b18497538cfa82bb
Marcin <mszczep1@wsisiz.edu.pl>
2018-09-26 12:16:28 +0200

    landing page with header and planing other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DescriptionComponent } from './description/description.component';
+import { MockupComponent } from './mockup/mockup.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DescriptionComponent,
+    MockupComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
