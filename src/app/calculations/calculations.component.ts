35ed62e50493a648f5c04fce570b4c254fc47ef2
Marcin <mszczep1@wsisiz.edu.pl>
2018-10-16 12:38:52 +0200

    Component calculations works. Logo updated.

--- src/app/calculations/calculations.component.ts
+++ src/app/calculations/calculations.component.ts
@@ -4,6 +4,8 @@ import { Calculations } from './calculations.model';
 import { CalculationsService } from './calculations.service';
 import { MockupTracks } from '../mockup/mockup-tracks.model';
 import { MockupTracksService } from '../mockup/mockup-tracks.service';
+import { Tracks } from '../shared/tracks.model';
+import { TracksService } from '../shared/tracks.service';
 
 @Component({
   selector: 'app-calculations',
@@ -13,18 +15,38 @@ import { MockupTracksService } from '../mockup/mockup-tracks.service';
 })
 export class CalculationsComponent implements OnInit {
   mockupTracks: MockupTracks[];
+  tracks: Tracks[];
   calculates: Calculations[];
+  sumOfTracks: number = 0;
+  priceOfTrack: {id: number, price: number}[];
   constructor(private mockupTracksService: MockupTracksService,
-              private calculationsService: CalculationsService) { }
+              private calculationsService: CalculationsService,
+              private tracksService: TracksService) { }
 
   ngOnInit() {
     this.mockupTracks = this.mockupTracksService.getMockupTracks();
+    this.calculates = this.calculationsService.getCalculations();
+    this.tracks = this.tracksService.getTracks();
     this.calculateTracksPrice();
   }
 
   calculateTracksPrice() {
+    this.sumOfTracks=0;
+    this.priceOfTrack=[];
     for (var i = this.mockupTracks.length - 1; i >= 0; --i) {
-      //this.calculationsService.this.mockupTracks.pikoNumber
+      for (var j = this.calculates.length - 1; j >= 0; --j) {
+        if (this.calculates[j].pikoNumber==this.mockupTracks[i].pikoNumber) {
+          for (var k = this.tracks.length - 1; k >= 0; --k) {
+            if (this.tracks[k].pikoNumber==this.mockupTracks[i].pikoNumber) {
+              this.priceOfTrack.push({id: k,
+                                      price: this.calculates[j].price});
+            }
+          }
+
+          this.sumOfTracks
+            = ((this.sumOfTracks*10)+(this.calculates[j].price*10))/10;
+        };
+      }
     }
   }
 }
--- src/app/calculations/calculations.service.ts
+++ src/app/calculations/calculations.service.ts
@@ -4,8 +4,8 @@ import { Calculations } from './calculations.model';
 export class CalculationsService {
   //hardcoded sample tracks. In future will be loaded by JSON
   private calculations: Calculations[] = [
-    new Calculations('55201', 6),
-    new Calculations('55212', 8)
+    new Calculations('55201', 6.90),
+    new Calculations('55212', 7.90)
   ];
 
   constructor() { }
